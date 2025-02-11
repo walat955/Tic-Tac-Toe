2013078a83b7a0fb6c3a9846eb709936327f413b
walat <ali.walat@mediatec.net>
2025-02-11 11:37:37 +0100

    more commint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,7 @@ export class AppComponent {
     this.winner$ = this.gameService.winner$;
     this.winningLine$ = this.gameService.winningLine$;
 
-    //ensure that the currentWinningLine is updated with the latest value emitted by ywinninLine$.
+    //ensure that the currentWinningLine is updated with the latest value emitted by winninLine$.
     /*
     here the subscribe method takes a single callback function as an argument,
     which will be executed whenever the observable emits a new value.
@@ -36,6 +36,7 @@ export class AppComponent {
     in summary this code sets up a subscription to the winningLine$ observable, ensuring that currentWinninLine is always
     updated with the latest value emitted by the observable.
     */
+   
     this.winningLine$.subscribe(line => this.currentWinningLine = line );
   }
 
@@ -49,6 +50,27 @@ export class AppComponent {
 
   //check if the index if a part of the currentWinningLine.
   isWinningSquare(index: number): boolean {
+    /**
+     * The ?. (optional chaining) ensures that if currentWinningLine is null or undefined, TypeScript won’t throw an error when calling .includes(index).
+     * If currentWinningLine is not null/undefined, .includes(index) checks whether index exists in the array.
+
+?? false ensure that the function always returns a boolean.
+If this.currentWinningLine?.includes(index) returns undefined or null, it defaults to false.
+Ensures the function always returns a boolean.
+     */
     return this.currentWinningLine?.includes(index) ?? false;
   }
+
+/**
+ * 
+ * this is the same method as above but more readable.
+ *  isWinningSquare(index: number): boolean {
+    if (!this.currentWinningLine) {
+      return false;
+    }
+    return this.currentWinningLine.includes(index);
+  }
+    If this.currentWinningLine is null or undefined, it returns false (ensuring it doesn’t crash).
+Otherwise, it checks if index exists in currentWinningLine (an array) and returns true or false.
+  */ 
 }
